ec79025d6bca22c8c78944695c6a30138265524b
chenming <583431043@qq.com>
2018-09-17 14:18:16 +0800

    修改数据库地址

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose'); //引入mongoose模块
-var db = mongoose.createConnection('mongodb://127.0.0.1:27017/chat');
+// var db = mongoose.createConnection('mongodb://127.0.0.1:27017/chat');
+
+
+var db = mongoose.createConnection('mongodb://kakaxicm:123456@47.105.128.141:27017/chat?authSource=admin');
 // ,{useMongoClient:true}
 db.on('error', function(error) {
 	console.log('数据库连接失败：' + error);
