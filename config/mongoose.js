73d448c8c7c066d11d36808124ebdd47872daad0
chenming <583431043@qq.com>
2018-09-17 15:48:25 +0800

    数据库log

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,10 +6,10 @@ var db = mongoose.createConnection('mongodb://127.0.0.1:27017/chat', {useNewUrlP
 // ,{useMongoClient:true}
 db.on('error', function(error) {
 	// console.log('数据库连接失败：' + error);
-    vfglobal.MyLog("sent:", "数据库连接失败"+error);
+    vfglobal.MyLog("sent:", "db failed"+error);
 });
 db.on('open', function() {
-    vfglobal.MyLog("sent:", "——数据库连接成功！——"+new Date().toLocaleString());
+    vfglobal.MyLog("sent:", "——db succ——"+new Date().toLocaleString());
 	// console.log('——数据库连接成功！——' + new Date().toLocaleString());
 });
 
